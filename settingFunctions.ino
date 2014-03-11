1f1508f397154772aab125b9046ed4c9dc3c41be
thetabor <tabor.henderson@gmail.com>
2014-03-11 11:13:05 -0600

    Update settingFunctions.ino
    Reliably loading and saving settings!

--- settingFunctions.ino
+++ settingFunctions.ino
@@ -2,18 +2,23 @@ void getSetting()
 {
   mySettings = SD.open("settings.txt");
   countSettings();
+  Serial.print("Counted ");
+  Serial.println(count);
+  Serial.println(freeRam());
   //These settings are for the settings loader
-  String settingstring = " ";
-  String newsettingstring = " ";
-  String pair = " ";
-  String setting = " ";
-  String value = " ";
+  String settingstring;
+  //String pair;
+  //String setting;
+  //String value;
   boolean haveSetting = false;
   while(mySettings.available()) {
     char character = mySettings.read();
     if(character == '/') {
-      while(character != '\n'){
+      while(mySettings.available()){
         character = mySettings.read();
+        if(character == '\n') {
+          break;
+        };
       };
     } 
     else {
@@ -22,82 +27,100 @@ void getSetting()
   };  
   // close the file:
   mySettings.close();
+  Serial.println(settingstring);
+  Serial.println(freeRam());
   String settings[count];
   String values[count];
-  int thisLineIndex = 1;
+  int thisLineIndex = 0;
   int nextLineIndex = settingstring.indexOf('\n');
-  for (int i = 0; i < count; i = i + 1){
+  int counter = count;
+  int i = 0;
+  while(counter > 0){
+    Serial.println(freeRam());
     if(!settingstring){
-      //Serial.println("Settings not found");
+      Serial.println("Settings not found");
       return;
     };
-    //Serial.println(thisLineIndex);
-    //Serial.println(nextLineIndex);
-    pair=settingstring.substring(thisLineIndex, nextLineIndex);
+    Serial.println(thisLineIndex);
+    Serial.println(nextLineIndex);
+    String pair=settingstring.substring(thisLineIndex, nextLineIndex);
     int nextnextlineIndex= settingstring.indexOf('\n', nextLineIndex+1);
     pair.trim();
     Serial.println(pair);
     thisLineIndex = nextLineIndex;
     nextLineIndex = nextnextlineIndex;
-    //Serial.println(thisLineIndex);
-    //Serial.println(nextLineIndex);
-    //Serial.println(newsettingstring);
+    Serial.println(thisLineIndex);
+    Serial.println(nextLineIndex);
     int eqIndex=pair.indexOf('=');
-    //Serial.println(eqIndex);
+    Serial.println(eqIndex);
     // comment check
     if(pair.startsWith("/")) {
       //Serial.println("Ignoring comment");
-      //count=count-1;
     }
     else if((eqIndex==0) || (eqIndex==-1)) {
-      //Serial.println("Settings format error, skipping");
-      //count=count-1;  
+      Serial.println("Settings format error, skipping");
     } 
     else if((0<eqIndex) && (eqIndex<pair.length())){
-      //Serial.println("Setting pair loaded ok");
+      Serial.println("Setting pair loaded ok");
       int valueIndex=pair.indexOf("=");
-      setting=String(pair.substring(0, valueIndex));
+      String setting=pair.substring(0, valueIndex-1);
       setting.trim();
-      value=String(pair.substring(valueIndex+1));
+      String value=pair.substring(valueIndex+1);
       value.trim();
-      settings[i]=String(setting);
-      values[i]=String(value);
+      settings[i]=setting;
+      values[i]=value;
       pair = " ";
       setting = " ";
       value = " ";
-      /*Serial.print("Found setting: ");
+      Serial.print("Found setting: ");
       Serial.print(i); 
       Serial.print(" : ");
       Serial.print(settings[i]);
       Serial.print(" : ");
-      Serial.println(values[i]);*/
+      Serial.println(values[i]);
       //haveSetting = true;
+      if(settings[i].startsWith("PingC")) {
+        PingCount = values[i].toInt();
+      } 
+      else if(settings[i].startsWith("PingD")) {
+        PingDelay = values[i].toInt();
+      } 
+      else if(settings[i].startsWith("PingM")) {
+        PingMode = values[i].toInt();
+      } 
+      else if(settings[i].startsWith("Time")) {
+        //note: with time as an int the max magnitude value is 32,768
+        //an if loop measuring the length of String values[i] combined with 
+        //a new long and two toInt calls should allow all values up to 
+        //the long memory limit (32 bits)
+        RunTime = values[i].toInt();
+      } 
+      else if(settings[i].startsWith("AccelM")) {
+        AccelMode = values[i].toInt();
+      } 
+      else if(settings[i].startsWith("DataM")) {
+        DataMode = values[i].toInt();
+      } 
+      else if(settings[i].startsWith("loops")) {
+        loops = values[i].toInt();
+      };
+      i++;
     } 
     else {
-      //Serial.println("Other error encountered, skipping line");
+      Serial.println("Other error encountered, skipping line");
+      count = count -1;
     };
-    if(settings[i].startsWith("PingCount")) {
-      PingCount = values[i].toInt();
-    } 
-    else if(settings[i].startsWith("PingDelay")) {
-      PingDelay = values[i].toInt();
-    } 
-    else if(settings[i].startsWith("PingMode")) {
-    } 
-    else if(settings[i].startsWith("Time")) {
-      Time = values[i].toInt();
-    } 
-    else if(settings[i].startsWith("AccelMode")) {
-    } 
-    else if(settings[i].startsWith("DataMode")) {
-    }
+    counter = counter - 1;
   }
-  Serial.print("Found ");
-  Serial.print(count);
-  Serial.print(" settings on ");
-  Serial.print(linecount);
-  Serial.println(" lines.");
-  settingstring=" ";
+  if(nextLineIndex == -1) {
+    Serial.print("Found ");
+    Serial.print(count);
+    Serial.print(" settings on ");
+    Serial.print(linecount);
+    Serial.println(" lines.");
+    settingstring=" ";
+    return;
+  };
 }
 
 void countSettings()
@@ -108,23 +131,28 @@ void countSettings()
   while(mySettings.position() < mySettings.size()){
     character = mySettings.read();
     if(character == '/') {
-      while(character != '\n'){
+      while((character != '\n') && (mySettings.available())){
         character = mySettings.read();
       };
     } 
-    else {
-    if(character == '='){// set "operator"- count as setting
+    else if(character == '='){// set "operator"- count as setting
       //Serial.println("counting setting");
       count++;
     } 
     else if (character == '\n'){ //newline- count it
       linecount++;  
-    }; 
+    };
   };
-  };
-  //tack on one more to the counts to make sure we get the last line
   mySettings.seek(0);
 }
+
+
+
+
+
+
+
+
 
 
 
