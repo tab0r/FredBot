edcfd7d7eae55ff336c4f643071b01fffe188c8a
thetabor <tabor.henderson@gmail.com>
2014-04-04 11:41:53 -0600

    Major update
    Removed accelerometer temporarily, streamlined settings loader, pythagorean correction for ping, still returning odd data.

--- SensorSettings.ino
+++ SensorSettings.ino
@@ -1,58 +1,27 @@
-/* FredBot Sensor Datalogger by Tabor Henderson
- 
- using Ping))) sensor, ADXL345 accelerometer and an SD-shield
- See additional notes below code
- 
-*/
-#include <SD.h>
-#include <ctype.h>
-//Add the SPI library so we can communicate with the ADXL345 sensor
-#include <SPI.h>
-File mySettings;
-File myData;
-unsigned long time;
-// this constant won't change.  It's the pin number
-// of the sensor's output:
-const int pingPin = 7;
-// Setting for SD-card reader
-// note that SD reader and ADXL require a chip select variable
-const int chipSelectSD = 10;
-//Assign the ADXL Chip Select signal to pin 10.
-const int chipSelectAC=9;
-//This is a list of some of the registers available on the ADXL345.
-//To learn more about these and the rest of the registers on the ADXL345, read the datasheet!
-char POWER_CTL = 0x2D;	//Power Control Register
-char DATA_FORMAT = 0x31;
-char DATAX0 = 0x32;	//X-Axis Data 0
-char DATAX1 = 0x33;	//X-Axis Data 1
-char DATAY0 = 0x34;	//Y-Axis Data 0
-char DATAY1 = 0x35;	//Y-Axis Data 1
-char DATAZ0 = 0x36;	//Z-Axis Data 0
-char DATAZ1 = 0x37;	//Z-Axis Data 1
-//This buffer will hold values read from the ADXL345 registers.
-char values[10];
-//These variables will be used to hold the x,y and z axis accelerometer values.
-int x,y,z;
-/* These settings are the ones that FredBot (should) know how to use
- Mode conventions
- -1: default off
- 0: off
- 1: on
- 2: other
- */
-int linecount = 0;
-int count = 0;
-int PingMode; // 1: use PingCount 2: use time
-int PingCount; // for mode 1
-int PingDelay; // for ALL modes- interval between readings
-int RunTime;  // for mode 2 
-int AccelMode; // 1: use ping settings 2: use Time 
-int DataMode; // for something I haven't made up yet; maybe a pre calculation or interpolation?
-int loops;
+//this version of the FredBot program is dedicated to the Ping))) ultrasonic rangefinder
+//parameters are blah blah and blah
 
-void setup()
-{
-  // Open serial communications and wait for port to open:
+#include <SD.h>
+
+unsigned long time;
+File myFile;
+// this constant won't change.  It's the pin number
+// of the distance sensor's output:
+const int pingPin = 7;
+// these two are for the alternate sensor 
+// which is much cheaper than Ping)))
+const int trigPin = 7;
+const int echoPin = 8;
+File mySettings;
+const int chipSelectSD = 10; 
+unsigned int PingM;
+unsigned int PingC;
+unsigned int PingD;
+unsigned int RunTime;
+unsigned int FBLoops;
+int DataM;
+
+void setup() {
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
@@ -60,93 +29,57 @@ void setup()
   Serial.println("Starting...");
   pinMode(10, OUTPUT);
   if (!SD.begin(chipSelectSD)) {
-    Serial.println("SD initialization failed!");
-    return;
+    Serial.println("SD initialization failed! Loading defaults...");
+    DataM = 0;
+    PingM = 0;
+    PingC = 500;
+    PingD = 1;
+    RunTime = 0;
+    FBLoops = 1;
+    //return;
+  } else {
+  getSettings();
   }
-  getSetting();
-  if(PingMode) {
-    Serial.print("Found PingMode "); 
-    Serial.println(PingMode);
-  }
-  if(PingCount) {
-    Serial.print("Found PingCount "); 
-    Serial.println(PingCount);
-  }
-  if(PingDelay) {
-    Serial.print("Found PingDelay "); 
-    Serial.println(PingDelay);
-  }
-  if(RunTime) {
-    Serial.print("Found RunTime "); 
-    Serial.println(RunTime);
-  }
-  if(AccelMode) {
-    Serial.print("Found AccelMode "); 
-    Serial.println(AccelMode);
-  }
-  if(DataMode) {
-    Serial.print("Found DataMode "); 
-    Serial.println(DataMode);
-  }
-  if(loops) {
-    Serial.print("Found loops "); 
-    Serial.println(loops);
-  }
-}
-
-void loop() {
-  //get iterator value and loop that many times
-  while(loops > 0) {
-    Serial.println("Looping! Wheee");
-    // open the data file
-    myData = SD.open("data.txt", FILE_WRITE);
+  Serial.println("Settings imported successfully!");
+  Serial.println();
+  //collect data based on settings parameters
+  //now we move onto doing stuff with our fresh settings
+  //to start, use DataM to determine how we want to handle data
+  switch (DataM) {
+  case -1:
+    //do nothing!
+    Serial.println("Doing nothing");
+    break;
+  case 0:
+    //print sensor readings to serial, throw them away
+    pingstuff(PingC, PingD, 0);  
+    break;
+  case 1:
+    //read ping based on parameters, print readings to serial and save
+    myFile = SD.open("data.txt", FILE_WRITE);
     // if the file opened okay, jump to main loop and write to it:
-    if ((myData) && (DataMode!=-1)) {
-      Serial.println("Doing stuff!");
-      //feelstuff();
-      //pingstuff();
-      myData.close(); 
+    if (myFile) {
+      //do stuff!
+      pingstuff(PingC, PingD, 1);
+      myFile.close(); 
     } 
     else {
       // if the file didn't open, print an error:
       Serial.println("error opening data.txt");
     }
-    myData.close(); 
-    loops = loops - 1;
+    break;
+  case 2:
+    //read accel based on parameters, print readings and save
+    break;
+  case 3:
+    //read both sensors continously, printing output
+    break;
+  case 4:
+    //read sensors based on parameters, print readings and save
+    break;
   }
 }
 
-/* This sketch is a gigantic hack! Using tutorial code for the most part, and on 20140217, attempting to add a settings header by JurgenG.
- 
- Ping))) Sensor
- 
- This sketch reads a PING))) ultrasonic rangefinder and returns the
- distance to the closest object in range. To do this, it sends a pulse
- to the sensor to initiate a reading, then listens for a pulse 
- to return.  The length of the returning pulse is proportional to 
- the distance of the object from the sensor.
- 
- The circuit:
- 	* +V connection of the PING))) attached to +5V
- 	* GND connection of the PING))) attached to ground
- 	* SIG connection of the PING))) attached to digital pin 7
- 
- http://www.arduino.cc/en/Tutorial/Ping
- 
- created 3 Nov 2008
- by David A. Mellis
- modified 30 Aug 2011
- by Tom Igoe
- 
- This example code is in the public domain.
- 
- */
-//this is a handy debugging function
-int freeRam () 
+void loop()
 {
-  extern int __heap_start, *__brkval; 
-  int v; 
-  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval); 
 }
-
-
